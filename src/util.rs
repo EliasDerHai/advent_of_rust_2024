761fdc2bad3def6b7d1f179b9ee3d23748479c26
EliasDerHai <eliashaider95@gmail.com>
2024-12-20 14:33:41 +0100

    git init

---
+++ src/util.rs
@@ -0,0 +1,13 @@
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn read_lines<P>(filename: P) -> io::Result<Vec<String>>
+    where
+        P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let buf_reader = io::BufReader::new(file);
+    buf_reader.lines().collect()
+}
