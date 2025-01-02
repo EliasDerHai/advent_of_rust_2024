031d3d84772326e9baf753295e4262339417baf1
EliasDerHai <eliashaider95@gmail.com>
2025-01-02 14:30:53 +0100

    solve day3 part2 (two solutions)

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,2 +1,42 @@
 pub mod part1;
 pub mod part2;
+pub mod part2_eager;
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use std::time::Instant;
+
+    use crate::day03::part2::solve_day_03_part_02;
+    use crate::day03::part2_eager::solve_day_03_part_02_eager;
+    use crate::util::{read_chars, read_string};
+
+    /// used for debugging an issue in my state-machine
+    #[test]
+    fn should_be_equal() {
+        let input = "do()mul(135,70)";
+
+        let lazy = solve_day_03_part_02(input.bytes().map(|b| b as char));
+        let eager = solve_day_03_part_02_eager(input);
+
+        assert_eq!(lazy, eager);
+    }
+
+    /// performance test - turn out eagerly loading String into mem and processing it with 'starts-with' is faster - than the char-by-char state-machine
+    #[test]
+    fn should_be_fast() {
+        let start2 = Instant::now();
+        let input2 = read_string("./src/day03/input.txt").unwrap();
+        let solution2 = solve_day_03_part_02_eager(input2.as_str());
+        let elapsed2 = start2.elapsed();
+        println!("Eager result: {solution2}, time: {elapsed2:?}");
+
+        let start1 = Instant::now();
+        let input1 = read_chars("./src/day03/input.txt").unwrap();
+        let solution1 = solve_day_03_part_02(input1);
+        let elapsed1 = start1.elapsed();
+        println!("Lazy result: {solution1}, time: {elapsed1:?}");
+    }
+}
--- src/util.rs
+++ src/util.rs
@@ -20,3 +20,14 @@ pub fn read_chars<P>(filename: P) -> io::Result<impl Iterator<Item=char>>
         .map(|buf_reader| buf_reader.bytes()
             .map(|byte| byte.unwrap() as char))
 }
+
+pub fn read_string<P>(filename: P) -> io::Result<String>
+    where
+        P: AsRef<Path>,
+{
+    let mut content = String::new();
+    let file = File::open(filename)?;
+    let mut buf_reader = io::BufReader::new(file);
+    buf_reader.read_to_string(&mut content)?;
+    Ok(content)
+}
