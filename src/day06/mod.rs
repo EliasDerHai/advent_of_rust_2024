1262a839f2431f2e1677e2ad3b07b5cf0fd96f44
EliasDerHai <eliashaider95@gmail.com>
2025-01-13 20:16:57 +0100

    refac day 6 part 2 replace hashmap with 2d-array

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -70,26 +70,6 @@ fn parse_to_map(input: String) -> (GameMap, GamePosition) {
     (map, start_pos)
 }
 
-fn travel_one_unit(map: &GameMap, pos: &GamePosition, dir: &Direction) -> (GamePosition, FieldType) {
-    let x = pos.0;
-    let y = pos.1;
-    let next: GamePosition = match dir {
-        N => {
-            (x, y - 1)
-        }
-        E => {
-            (x + 1, y)
-        }
-        S => {
-            (x, y + 1)
-        }
-        W => {
-            (x - 1, y)
-        }
-    };
-    map.get(&next).map(|&field_type| (next, field_type)).unwrap_or((next, Out))
-}
-
 /// traverses the map from a given start position and returns the count of fields that have been visited (only first visit counts) until the map's end is reached
 /// navigation strategy: walk straight till obstacle or end of map / turn right if obstacle
 fn traverse_map(map: GameMap, start_pos: GamePosition) -> isize {
@@ -116,12 +96,121 @@ fn traverse_map(map: GameMap, start_pos: GamePosition) -> isize {
     visited.len() as isize
 }
 
-
 /// general strategy: read the map into a suitable data structure, walk the maze according to the
 /// navigation rules and count each visited field (no duplicates = hashset)
 pub fn solve_day_06_part_01(input: String) -> isize {
     let (map, start_pos) = parse_to_map(input);
     traverse_map(map, start_pos)
+}
+
+type Game2DArray = [[FieldType; MAX_MAP_SIZE]; MAX_MAP_SIZE];
+
+type LowMemGamePosition = (u8, u8);
+
+
+/// map is actually square and 130 fields wide/tall
+const MAX_MAP_SIZE: usize = 130;
+
+fn parse_to_2d_arr(input: String) -> (Game2DArray, LowMemGamePosition) {
+    let mut map: Game2DArray = [[Out; MAX_MAP_SIZE]; MAX_MAP_SIZE];
+    let mut start_pos = (u8::MAX, u8::MAX);
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            match c {
+                '#' => {
+                    map[x][y] = Occupied;
+                }
+                '.' => {
+                    map[x][y] = Free;
+                }
+                '^' => {
+                    map[x][y] = Free;
+                    start_pos = (x as u8, y as u8);
+                }
+                _ => panic!("Unexpected token '{}' in input data", c),
+            }
+        }
+    }
+
+    if start_pos == (u8::MAX, u8::MAX) {
+        panic!("No start position found!")
+    }
+    (map, start_pos)
+}
+
+fn travel_one_unit_fast(map: &Game2DArray, pos: &LowMemGamePosition, dir: &Direction) -> (LowMemGamePosition, FieldType) {
+    let mut x = pos.0 as i16;
+    let mut y = pos.1 as i16;
+    match dir {
+        N => {
+            y -= 1;
+        }
+        E => {
+            x += 1;
+        }
+        S => {
+            y += 1;
+        }
+        W => {
+            x -= 1;
+        }
+    };
+
+    if x < 0 || x >= MAX_MAP_SIZE as i16 || y < 0 || y >= MAX_MAP_SIZE as i16 {
+        ((u8::MAX, u8::MAX), Out)
+    } else {
+        ((x as u8, y as u8), map[x as usize][y as usize])
+    }
+}
+
+
+fn travel_one_unit(map: &GameMap, pos: &GamePosition, dir: &Direction) -> (GamePosition, FieldType) {
+    let x = pos.0;
+    let y = pos.1;
+    let next: GamePosition = match dir {
+        N => {
+            (x, y - 1)
+        }
+        E => {
+            (x + 1, y)
+        }
+        S => {
+            (x, y + 1)
+        }
+        W => {
+            (x - 1, y)
+        }
+    };
+    map.get(&next).map(|&field_type| (next, field_type)).unwrap_or((next, Out))
+}
+
+/// checks if a map loops or not
+fn check_for_loop_fast(
+    map: &Game2DArray,
+    mut pos: LowMemGamePosition,
+    mut dir: Direction,
+    mut visited: HashSet<(LowMemGamePosition, Direction)>,
+) -> bool {
+    loop {
+        let (mut next_pos, mut next_field) = travel_one_unit_fast(&map, &pos, &dir);
+        while next_field == Occupied {
+            dir = dir.next();
+            (next_pos, next_field) = travel_one_unit_fast(&map, &pos, &dir);
+        }
+        match next_field {
+            Free => {
+                if visited.contains(&(next_pos, dir)) {
+                    return true;
+                } else {
+                    visited.insert((next_pos, dir));
+                    pos = next_pos;
+                }
+            }
+            Out => return false,
+            Occupied => panic!("Should not be possible..."),
+        }
+    }
 }
 
 /// checks if a map loops or not
@@ -155,6 +244,48 @@ fn check_for_loop(
     }
 }
 
+
+/// traverses the map while also evaluating if a new obstacle in front of the current pos would
+/// introduce a loop
+/// these obstacles can only be put on not traveled fields, since otherwise the traveled path would
+/// not be possible
+fn traverse_map_with_obstacle_loops_fast(
+    mut map: Game2DArray,
+    start_pos: LowMemGamePosition,
+    mut visited: HashSet<(LowMemGamePosition, Direction)>,
+) -> usize {
+    let mut pos = Some(start_pos);
+    let mut dir = N;
+    let mut obstacles_for_loop = 0;
+    while let Some((x, y)) = pos {
+        let (mut next_pos, mut next_field) = travel_one_unit_fast(&map, &(x, y), &dir);
+        while next_field == Occupied {
+            dir = dir.next();
+            (next_pos, next_field) = travel_one_unit_fast(&map, &(x, y), &dir);
+            // println!("Hit obstacle at ({x}, {y}) - changing dir to {:?}", dir);
+        }
+        if next_field == Out {
+            // println!("Leaving map on {:?}", next_pos);
+            pos = None; // terminates while loop
+        } else { // must be free
+            if !visited.iter().any(|f| f.0 == next_pos) {
+                // let mut modified_map = map.clone();
+                map[next_pos.0 as usize][next_pos.1 as usize] =  Occupied; // mutate instead of clone - perf. optimization
+                let visited_copy = visited.clone();
+                if check_for_loop_fast(&map, pos.unwrap(), dir.next(), visited_copy) {
+                    obstacles_for_loop += 1;
+                }
+                map[next_pos.0 as usize][next_pos.1 as usize] =  Free; // revert to original state - perf. optimization
+            }
+            // println!("at {:?}", next_pos);
+            pos = Some(next_pos);
+            visited.insert((next_pos, dir));
+        }
+    }
+    // println!("obstacles: {:?}", obstacles_for_loop);
+    obstacles_for_loop
+}
+
 /// traverses the map while also evaluating if a new obstacle in front of the current pos would
 /// introduce a loop
 /// these obstacles can only be put on not traveled fields, since otherwise the traveled path would
@@ -172,10 +303,8 @@ fn traverse_map_with_obstacle_loops(
         while next_field == Occupied {
             dir = dir.next();
             (next_pos, next_field) = travel_one_unit(&map, &(x, y), &dir);
-            // println!("Hit obstacle at ({x}, {y}) - changing dir to {:?}", dir);
         }
         if next_field == Out {
-            // println!("Leaving map on {:?}", next_pos);
             pos = None; // terminates while loop
         } else { // must be free
             if !visited.iter().any(|f| f.0 == next_pos) {
@@ -208,9 +337,16 @@ pub fn solve_day_06_part_02(input: String) -> usize {
     traverse_map_with_obstacle_loops(map, start_pos, visited)
 }
 
+pub fn solve_day_06_part_02_fast(input: String) -> usize {
+    let (map, start_pos) = parse_to_2d_arr(input);
+    // println!("Map: {:?}", map);
+    let visited: HashSet<(LowMemGamePosition, Direction)> = HashSet::from([(start_pos, N)]);
+    traverse_map_with_obstacle_loops_fast(map, start_pos, visited)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day06::{solve_day_06_part_01, solve_day_06_part_02};
+    use crate::day06::{solve_day_06_part_01, solve_day_06_part_02_fast};
     use crate::day06::Direction::*;
     use crate::util::read_string;
 
@@ -253,12 +389,22 @@ mod tests {
     fn should_solve_day_06_part_02() {
         let input = read_string("./src/day06/input.txt").unwrap();
 
-        let solution = solve_day_06_part_02(input);
+        let solution = solve_day_06_part_02_fast(input);
 
         println!("{solution}");
-        assert_eq!(2262, solution); // on avg ~ 6.7s before optimization
+        assert_eq!(2262, solution);
+        // on avg ~ 6.7s before optimization
+        // on avg ~ 5.4s after switching to 2d-arr instead of hashmap
     }
 
+
+    #[test]
+    fn should_find_dimensions_of_input() {
+        let input = read_string("./src/day06/input.txt").unwrap();
+        let rows = input.lines().count();
+        let cols = input.lines().next().unwrap().len();
+        println!("{cols} - {rows}");
+    }
 
     #[test]
     fn should_solve_day_06_part_02_sample() {
@@ -274,6 +420,6 @@ mod tests {
 #.........
 ......#...".trim().to_string();
 
-        assert_eq!(6, solve_day_06_part_02(input));
+        assert_eq!(6, solve_day_06_part_02_fast(input));
     }
 }
