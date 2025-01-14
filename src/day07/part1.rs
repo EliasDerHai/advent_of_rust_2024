7cb15ee273002c2d7421f13d184649278e248052
EliasDerHai <eliashaider95@gmail.com>
2025-01-14 13:15:28 +0100

    solve day 7 part 2

--- src/day07/part1.rs
+++ src/day07/part1.rs
@@ -6,13 +6,15 @@ struct Calibration {
 enum Operator {
     Multiply,
     Add,
+    Concat,
 }
 
 impl Operator {
     fn apply(&self, left: usize, right: usize) -> usize {
         match self {
             Operator::Multiply => left * right,
-            Operator::Add => left + right
+            Operator::Add => left + right,
+            Operator::Concat => format!("{left}{right}").parse().unwrap()
         }
     }
 }
@@ -28,36 +30,57 @@ impl Calibration {
         }
     }
 
-    fn check_if_can_meet_target(&self) -> bool {
-        let mut iter = self.parts.iter();
-        let &init = iter.next().unwrap();
-        let options = iter.fold(vec![init], |acc, &next| {
-            let mut add = acc.iter().map(|&a| {
-                if a <= self.target {
-                    Operator::Add.apply(a, next)
-                } else {
-                    self.target + 1 // doesn't matter as long as operators are always incremental
-                }
-            }).collect::<Vec<usize>>();
-            let mut mul = acc.iter().map(|&a| {
-                if a <= self.target {
-                    Operator::Multiply.apply(a, next)
-                } else {
-                    self.target + 1 // doesn't matter as long as operators are always incremental
-                }
-            }).collect::<Vec<usize>>();
-            add.append(&mut mul);
-            add
+
+    fn check_if_can_meet_target(&self, include_concat: bool) -> bool {
+        let (&init, tail) = self.parts.split_first().expect("parts cannot be empty");
+        let final_values = tail.iter().fold(vec![init], |acc, &next| {
+            let mut results = Vec::with_capacity(acc.len() * 3);
+
+            acc.iter()
+                .map(|&a| {
+                    if a <= self.target {
+                        Operator::Add.apply(a, next)
+                    } else {
+                        self.target + 1
+                    }
+                })
+                .for_each(|val| results.push(val));
+
+            acc.iter()
+                .map(|&a| {
+                    if a <= self.target {
+                        Operator::Multiply.apply(a, next)
+                    } else {
+                        self.target + 1
+                    }
+                })
+                .for_each(|val| results.push(val));
+
+            if include_concat {
+                acc.iter()
+                    .map(|&a| {
+                        if a <= self.target {
+                            Operator::Concat.apply(a, next)
+                        } else {
+                            self.target + 1
+                        }
+                    })
+                    .for_each(|val| results.push(val));
+            }
+
+            results
         });
-        options.contains(&self.target)
+
+        // Check if we can meet the target
+        final_values.contains(&self.target)
     }
 }
 
-pub fn solve_day_07_part_01(input: String) -> usize {
+pub fn solve_day_07(input: String, include_concat: bool) -> usize {
     input
         .lines()
         .map(|l| Calibration::from_line(l))
-        .filter(|c| c.check_if_can_meet_target())
+        .filter(|c| c.check_if_can_meet_target(include_concat))
         .map(|c| c.target)
         .sum()
 }
@@ -72,7 +95,7 @@ mod tests {
     fn should_solve_day_07_part_01() {
         let input = read_string("./src/day07/input.txt").unwrap();
 
-        let solution = solve_day_07_part_01(input);
+        let solution = solve_day_07(input, false);
 
         println!("{solution}");
         assert_eq!(303766880536, solution);
@@ -91,10 +114,10 @@ mod tests {
 21037: 9 7 18 13
 292: 11 6 16 20".trim().to_string();
 
-        assert_eq!(3749, solve_day_07_part_01(input));
+        assert_eq!(3749, solve_day_07(input, false));
 
-        assert_eq!(3, solve_day_07_part_01("3: 1 1 1".to_string()));
+        assert_eq!(3, solve_day_07("3: 1 1 1".to_string(), false));
 
-        assert_eq!(10, solve_day_07_part_01("10: 2 5 1".to_string()));
+        assert_eq!(10, solve_day_07("10: 2 5 1".to_string(), false));
     }
 }
--- src/day07/part2.rs
+++ src/day07/part2.rs
@@ -1,27 +1,32 @@
-pub fn solve_day_07_part_02(input: String) -> u32 {
-    todo!()
-}
-
 #[cfg(test)]
 mod tests {
+    use crate::day07::part1::solve_day_07;
     use crate::util::read_string;
-
-    use super::*;
 
     #[test]
     fn should_solve_day_07_part_02() {
-        let input = read_string("./src/day_07/input.txt").unwrap();
+        let input = read_string("./src/day07/input.txt").unwrap();
 
-        let solution = solve_day_07_part_02(input);
+        let solution = solve_day_07(input, true);
 
         println!("{solution}");
+        assert_eq!(337041851384440, solution);
     }
 
 
     #[test]
     fn should_solve_day_07_part_02_sample() {
-        let input = "".trim().to_string();
+        let input = "
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20".trim().to_string();
 
-        assert_eq!(0, solve_day_07_part_02(input));
+        assert_eq!(11387, solve_day_07(input, true));
     }
 }
