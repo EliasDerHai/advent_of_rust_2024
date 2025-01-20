7e0a0a5244afb0ceca775d7091192c04a4bceec7
EliasDerHai <eliashaider95@gmail.com>
2025-01-20 10:49:10 +0100

    solve day 11 part 2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,53 +1,71 @@
-use std::thread::sleep;
-use std::time::Duration;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use rayon::prelude::*;
 
 struct MagicStones {
     stones: Vec<usize>,
+    stone_cache: Arc<Mutex<HashMap<(usize, u8), usize>>>, // just an experiment, but actually the
+    // single threaded variant (w.o. mutex, arc & rayon) is twice as fast, which makes sense
+    // considering that probably most of the time the cache is locked anyways...
 }
 
 impl From<&str> for MagicStones {
     fn from(value: &str) -> Self {
         MagicStones {
-            stones: value.split_whitespace().map(|n| n.parse::<usize>().expect("Nan")).collect()
+            stones: value.split_whitespace().map(|n| n.parse::<usize>().expect("Nan")).collect(),
+            stone_cache: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 }
 
 impl MagicStones {
-    fn evolve(mut self) -> Self {
-        self.stones = self.stones
-            .par_iter()
-            .flat_map(|s| {
-                match s {
-                    0 => vec![1],
-                    x  if x.to_string().len() % 2 == 0 => {
-                        let x_str = x.to_string();
-                        let split_idx = x_str.len() / 2;
-                        let left = &x_str[..split_idx];
-                        let right = &x_str[split_idx..];
-                        vec![left, right]
-                            .into_iter()
-                            .map(|str_val| str_val.parse::<usize>().unwrap())
-                            .collect()
-                    }
-                    _ => vec![*s * 2024]
-                }
-            })
-            .collect();
-        self
+    pub fn simulate(self, iterations: u8) -> usize {
+        self.stones
+            .clone()
+            .into_par_iter()
+            .map(|s| self.blink(s, iterations))
+            .sum()
+    }
+
+    fn blink(&self, element: usize, iterations: u8) -> usize {
+        if iterations == 0 {
+            return 1;
+        }
+
+        let hash_key = (element, iterations);
+        {
+            match Arc::clone(&self.stone_cache).lock().unwrap().get(&hash_key) {
+                None => {}
+                Some(c) => { return *c; }
+            }
+        }
+
+
+        let count = match element {
+            0 => {
+                self.blink(1, iterations - 1)
+            }
+            x  if x.to_string().len() % 2 == 0 => {
+                let x_str = x.to_string();
+                let split_idx = x_str.len() / 2;
+                let left = x_str[..split_idx].parse::<usize>().unwrap();
+                let right = x_str[split_idx..].parse::<usize>().unwrap();
+                self.blink(left, iterations - 1) + self.blink(right, iterations - 1)
+            }
+            _ => self.blink(element * 2024, iterations - 1)
+        };
+
+        {
+            Arc::clone(&self.stone_cache).lock().unwrap().insert(hash_key, count);
+        }
+
+        count
     }
 }
 
-pub fn solve_day_11_part_01(input: &str, generations: u8) -> Vec<usize> {
-    let mut stones = MagicStones::from(input);
-    for i in 0..generations {
-        stones = stones.evolve();
-        println!("... gen{i} done ...");
-        sleep(Duration::from_millis(50));
-    }
-    stones.stones
+pub fn solve_day_11(input: &str, generations: u8) -> usize {
+    MagicStones::from(input).simulate(generations)
 }
 
 #[cfg(test)]
@@ -57,32 +75,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn should_solve_day_11_part_01() {
+    fn should_solve_day_11() {
         let input = read_string("./src/day11/input.txt").unwrap();
 
-        let solution = solve_day_11_part_01(input.as_str(), 25).len();
+        let solution = solve_day_11(input.as_str(), 25);
 
         println!("{solution}");
+        assert_eq!(199982, solution);
     }
 
     #[test]
     fn should_solve_day_11_part_02() {
         let input = read_string("./src/day11/input.txt").unwrap();
 
-        let solution = solve_day_11_part_01(input.as_str(), 75).len();
+        let solution = solve_day_11(input.as_str(), 75);
 
+        assert_eq!(237149922829154, solution);
         println!("{solution}");
-    }
-
-    #[test]
-    fn should_solve_day_11_part_01_sample() {
-        let input = "125 17".trim();
-        let expected_gen1 = vec![253000, 1, 7];
-        let expected_gen2 = vec![253, 0, 2024, 14168];
-        let expected_gen3 = vec![512072, 1, 20, 24, 28676032];
-
-        assert_eq!(expected_gen1, solve_day_11_part_01(input, 1));
-        assert_eq!(expected_gen2, solve_day_11_part_01(input, 2));
-        assert_eq!(expected_gen3, solve_day_11_part_01(input, 3));
     }
 }
